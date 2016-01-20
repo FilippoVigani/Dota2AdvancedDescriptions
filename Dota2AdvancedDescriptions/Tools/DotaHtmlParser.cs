60967a7b1b0d6cc9b689c234284e7ef875b2f1f4
Filippo Vigani <pippoviga95@hotmail.it>
2016-01-20 22:09:21 +0100

    Added abilities parsing from gamepedia

---
+++ Dota2AdvancedDescriptions/Tools/DotaHtmlParser.cs
@@ -0,0 +1,38 @@
+﻿using Dota2AdvancedDescriptions.Properties;
+using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dota2AdvancedDescriptions.Tools
+{
+    public class DotaHtmlParser
+    {
+
+        public void ParseAbilitiesCastPoints(string url, string tableClass)
+        {
+            WebClient webClient = new WebClient();
+            string page = webClient.DownloadString(Settings.Default.CastPointsTableAddress);
+
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(page);
+
+            var nodes = doc.DocumentNode.SelectNodes(Settings.Default.CastPointsTableXPath);
+            HtmlNode node = nodes.ElementAt(Settings.Default.CastPointsTableIndex); 
+
+            Dictionary<string, Dictionary<string, string>> abilities = new Dictionary<string, Dictionary<string, string>>();
+            foreach (var row in node.Descendants(Settings.Default.Tr).Skip(1).Where(tr => tr.Elements(Settings.Default.Td).Count() > 1))
+            {
+                Dictionary<string, string> ability = new Dictionary<string, string>();
+                for (int i = 0; i < row.Elements(Settings.Default.Td).Count(); i++)
+                {
+                    ability.Add(node.Descendants(Settings.Default.Tr).ElementAt(0).Elements(Settings.Default.Th).ElementAt(i).InnerText.Trim(), row.Elements(Settings.Default.Td).ElementAt(i).InnerText.Trim());
+                }
+                abilities.Add(ability.ElementAt(0).Value, ability);
+            }
+        }
+    }
+}
---
+++ Dota2AdvancedDescriptions/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Dota2AdvancedDescriptions.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dota2AdvancedDescriptions
+{
+    public class MainWindowViewModel
+    {
+        private DotaHtmlParser _htmlParser;
+
+        public MainWindowViewModel()
+        {
+            _htmlParser = new DotaHtmlParser();
+            _htmlParser.ParseAbilitiesCastPoints(null, null);
+        }
+    }
+}
